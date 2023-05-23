07b49cf120ce3680f5dbdf0316c2750086255d66
Lorena Ponsico <lorena.poca@gmail.com>
2023-05-23 18:59:09 +0200

    first commit

---
+++ src/app/home/escena/escena.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escena',
+  templateUrl: './escena.component.html',
+  styleUrls: ['./escena.component.css']
+})
+export class EscenaComponent {
+  public phrase :string = "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial" 
+}
