ebc90772e9afaf2884075c9de62775aebe2eb10b
Lorena Ponsico <lorena.poca@gmail.com>
2023-05-29 19:12:52 +0200

    ex4 n1 previous and next methods to paint phrases

--- src/app/home/escena/escena.component.ts
+++ src/app/home/escena/escena.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { Text } from '../interfaces/text.interface';
 
 @Component({
@@ -7,15 +7,27 @@ import { Text } from '../interfaces/text.interface';
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
-  // public phrase :string = "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial" 
+ @Input()
+ public stories: Text[] = []
 
+ 
+  public currentSentence = 0;
 
-  @Input()
-  public Story: Text[] = [{
-    sentenceOne: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
-    sentenceTwo: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-    sentenceThree:  "L'heroi va decidir travessar la porta que el portava a casa",
-    sentenceFour: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
+  previous(): void {
+    if (this.currentSentence > 0 ) {
+     this.currentSentence-- 
+    }
+       }
+    
+    
+    next(): void{
+      if(this.currentSentence < 3){
+        this.currentSentence++;
+      }
+    }
+   
+    constructor(){
+     
+    }
+  }
 
-  }]
-}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Text } from './interfaces/text.interface';
 
 
 @Component({
@@ -8,5 +9,12 @@ import { Component, Input } from '@angular/core';
 })
 export class HomeComponent {
 
-
+  @Input()
+  public stories: Text[] = [{
+    sentenceOne: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    sentenceTwo: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    sentenceThree:  "L'heroi va decidir travessar la porta que el portava a casa",
+    sentenceFour: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
+    
+  }]
 }
