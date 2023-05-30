79052b9ecf75a46085cdce5818f487846bd475b9
Lorena Ponsico <lorena.poca@gmail.com>
2023-05-30 10:06:06 +0200

    ex5 n2 explanation message and css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Text } from './interfaces/text.interface';
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,13 +11,12 @@ export class HomeComponent {
   public stories: Text[] = [{
     sentenceOne: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
     sentenceTwo: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-    sentenceThree:  "L'heroi va decidir travessar la porta que el portava a casa",
+    sentenceThree: "L'heroi va decidir travessar la porta que el portava a casa",
     sentenceFour: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
-    
   }]
-  public showSection: boolean = false ;
- 
- 
+
+  public showSection: boolean = false;
+
   onShowSection() {
     this.showSection = true;
   }
